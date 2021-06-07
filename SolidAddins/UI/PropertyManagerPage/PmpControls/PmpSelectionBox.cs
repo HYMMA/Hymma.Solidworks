5ef0afed07b72092815d5f166ffed40e7d2e015d
Hymma.net <info@hymma.net>
2021-06-07 16:57:58 +1000

    PmpSelection changed
    added half of events

--- SolidAddins/UI/PropertyManagerPage/PmpControls/PmpSelectionBox.cs
+++ SolidAddins/UI/PropertyManagerPage/PmpControls/PmpSelectionBox.cs
@@ -1,4 +1,5 @@
 ﻿using SolidWorks.Interop.swconst;
+using System;
 
 namespace Hymma.SolidTools.SolidAddins
 {
@@ -10,9 +11,10 @@ namespace Hymma.SolidTools.SolidAddins
         /// <summary>
         /// default constructor
         /// </summary>
-        public PmpSelectionBox() : base(swPropertyManagerPageControlType_e.swControlType_Selectionbox)
+        public PmpSelectionBox(swSelectType_e[] Filter,  short Height=50) : base(swPropertyManagerPageControlType_e.swControlType_Selectionbox)
         {
-
+            this.Height = Height;
+            this.Filter = Filter;
         }
         /// <summary>
         /// array of <see cref="swSelectType_e"/> to allow selection of specific types only
@@ -23,5 +25,33 @@ namespace Hymma.SolidTools.SolidAddins
         /// height of this selection box in proerty manager page
         /// </summary>
         public short Height { get; set; }
+
+        /// <summary>
+        /// SOLIDWORKS will invoke this once focus is changed from this selection box
+        /// </summary>
+        public Action OnFocusChanged { get; set; }
+
+        public Action<int> OnListChanged { get; set; }
     }
+}
+
+
+public void OnSelectionboxFocusChanged(int Id)
+{
+    throw new NotImplementedException();
+}
+
+public void OnSelectionboxListChanged(int Id, int Count)
+{
+    throw new NotImplementedException();
+}
+
+public void OnSelectionboxCalloutCreated(int Id)
+{
+    throw new NotImplementedException();
+}
+
+public void OnSelectionboxCalloutDestroyed(int Id)
+{
+    throw new NotImplementedException();
 }
