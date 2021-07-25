03bbe0cd460eb0170cb1ff6433d6df11c5be5e75
HYMMA <71206865+HYMMA@users.noreply.github.com>
2021-07-26 06:11:45 +1000

    pmpWindowHandler

--- Addins/UI/PropertyManagerPage/PmpControls/PmpWindowHandler.cs
+++ Addins/UI/PropertyManagerPage/PmpControls/PmpWindowHandler.cs
@@ -1,4 +1,5 @@
 ﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
 using System;
 using System.Windows.Controls;
 using System.Windows.Forms.Integration;
@@ -8,14 +9,14 @@ namespace Hymma.SolidTools.Addins
     /// <summary>
     /// a windows form host that solidworks uses to show win forms or wpf
     /// </summary>
-    public class PmpWindowHandler : PmpControl, IEquatable<PmpWindowHandler>
+    public class PmpWindowHandler : PmpControl<IPropertyManagerPageWindowFromHandle> , IEquatable<PmpWindowHandler>
     {
         /// <summary>
         /// default constructor 
         /// </summary>
         /// <param name="ElementHost">solidworks uses this object to hook into a windows form</param>
         /// <param name="WinFormOrWpfControl">a windows form or wpf controller</param>
-        public PmpWindowHandler(ElementHost ElementHost, UserControl WinFormOrWpfControl) : base(SolidWorks.Interop.swconst.swPropertyManagerPageControlType_e.swControlType_WindowFromHandle)
+        public PmpWindowHandler(ElementHost ElementHost, UserControl WinFormOrWpfControl) : base(swPropertyManagerPageControlType_e.swControlType_WindowFromHandle)
         {
             this.ElementHost = ElementHost;
             this.WindowsControl = WinFormOrWpfControl;
@@ -38,8 +39,12 @@ namespace Hymma.SolidTools.Addins
         /// a handle to connect <see cref="WindowsControl"/> to solidworks property manager page
         /// </summary>
         public IPropertyManagerPageWindowFromHandle ProperptyManagerPageHandle { get; internal set; }
-        
-        //we have to make sure each SwWindowHandler has its unique ElementHost
+
+        /// <summary>
+        /// makes sure each SwWindowHandler has its unique ElementHost
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public bool Equals(PmpWindowHandler other)
         {
             if (other is null) return false;
