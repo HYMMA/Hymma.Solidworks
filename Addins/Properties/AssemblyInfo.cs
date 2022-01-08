1efd9b0f1a65d927b39d02d373f7a5c338fd7c57
HYMMA <71206865+HYMMA@users.noreply.github.com>
2022-01-08 22:12:39 +1100

    renamed to solidworks.addins

--- Addins/Properties/AssemblyInfo.cs
+++ Addins/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hymma.SolidTools.Addins")]
+[assembly: AssemblyTitle("Hymma.Solidworks.Addins")]
 [assembly: AssemblyDescription("Makes addin for solidworks")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HYMMA")]
-[assembly: AssemblyProduct("Hymma.SolidTools.Addins")]
+[assembly: AssemblyProduct("Hymma.Solidworks.Addins")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("HYMMA")]
 [assembly: AssemblyCulture("")]
--- Addins/Properties/Resources.Designer.cs
+++ Addins/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Hymma.SolidTools.Addins.Properties {
+namespace Hymma.Solidworks.Addins.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Hymma.SolidTools.Addins.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Hymma.SolidTools.Addins.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Hymma.Solidworks.Addins.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
