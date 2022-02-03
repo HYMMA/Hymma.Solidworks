b0107f09b52876ce43d9ff9ee209e03bed2e01db
HYMMA <71206865+HYMMA@users.noreply.github.com>
2022-02-03 22:58:44 +1100

    remove redundant usings

--- Addins.Fluent/Core/Delegates.cs
+++ Addins.Fluent/Core/Delegates.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hymma.Solidworks.Addins.Fluent
+﻿namespace Hymma.Solidworks.Addins.Fluent
 {
     /// <summary>
     /// Equivalent to Aciton&lt;bool&gt;
