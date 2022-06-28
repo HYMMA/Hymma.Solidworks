7b594bdc24c40bff670c8a9453b7f2eef32032d3
H-Ashrafi <ashrafi@outlook.com.au>
2022-06-28 23:32:27 +1000

    remove logger from registry helper
    previously registry helper was logging everything and also was responsible for registering a logger into windows event viewr. this could cause the whole build process crash for some reason, so we simply remove the loggin mechanisms from the library for now and let the client add their own loggers

--- Addins/Helpers/RegisteryHelper.cs
+++ Addins/Helpers/RegisteryHelper.cs
@@ -11,107 +11,51 @@ namespace Hymma.Solidworks.Addins.Helpers
 {
     internal static class RegisteryHelper
     {
-
-        internal static void Register(Type t)
+        /// <summary>
+        /// registers <see cref="Type"/> provided to RegisteryHelper so solidworks can find it
+        /// </summary>
+        /// <param name="type">type of class that inherits from  <see cref="AddinMaker"/></param>
+        public static void BaseRegisterFunction(Type type)
         {
-            var addinAttribute = t.TryGetAttribute<AddinAttribute>(false) as AddinAttribute;
-
-            if (addinAttribute == null)
-                return;
-
             try
             {
-                Logger.Source = "Solidworks Addin";
-                Log("trying to register");
-
-                string keyname = "SOFTWARE\\SolidWorks\\Addins\\{" + t.GUID.ToString() + "}";
+                var addinAttribute = type.TryGetAttribute<AddinAttribute>(false);
+                string keyname = "SOFTWARE\\SolidWorks\\Addins\\{" + type.GUID.ToString() + "}";
                 RegistryKey addinKey = Registry.LocalMachine.CreateSubKey(keyname);
                 addinKey.SetValue(null, 0);
 
                 addinKey.SetValue("Description", addinAttribute.Description);
                 addinKey.SetValue("Title", addinAttribute.Title);
 
-                keyname = "Software\\SolidWorks\\AddInsStartup\\{" + t.GUID.ToString() + "}";
+                keyname = "Software\\SolidWorks\\AddInsStartup\\{" + type.GUID.ToString() + "}";
                 RegistryKey addinStartUpKey = Registry.CurrentUser.CreateSubKey(keyname);
                 addinStartUpKey.SetValue(null, Convert.ToInt32(addinAttribute.LoadAtStartup), RegistryValueKind.DWord);
 
-                #region Extract icon during registration
-                _addinTitle = addinAttribute.Title;
-                addinKey.SetValue("Icon Path", GetIconPath(t, addinAttribute));
-
-                Log("Registration was successful!");
-                #endregion
+                AddinIcons.Instance().SaveAddinIcon(type, out string iconPath);
+                addinKey.SetValue("Icon Path", iconPath);
             }
-            catch (System.NullReferenceException e)
+            catch (Exception)
             {
-                Log($"Error! There was a problem registering this library: addinModel is null. \n\"" + e.Message + "\"");
             }
-            catch (System.Exception e)
-            {
-                Log(e);
-            }
-        }
-
-        internal static void Unregister(Type type)
-        {
-            Logger.Source = "Solidworks Addin";
-            var swAttr = t.TryGetAttribute<AddinAttribute>(false) as AddinAttribute;
-            if (swAttr == null)
-                try
-                {
-                    Log("trying to unregister");
-                    string keyname = "SOFTWARE\\SolidWorks\\Addins\\{" + t.GUID.ToString() + "}";
-                    Registry.LocalMachine.DeleteSubKey(keyname);
-
-                    keyname = "Software\\SolidWorks\\AddInsStartup\\{" + t.GUID.ToString() + "}";
-                    Registry.CurrentUser.DeleteSubKey(keyname);
-
-                    Log("Unregister successful");
-                    //UnRegisterLogger(swAttr.Title);
-
-                }
-                catch (System.NullReferenceException nl)
-                {
-                    Log(nl.Message);
-
-                    //TODO:log this
-                    Console.WriteLine("Error! There was a problem unregistering this library: " + nl.Message);
-                }
-                catch (System.Exception e)
-                {
-                    //TODO:log this
-                    Log(e.Message);
-                    Console.WriteLine("Error! There was a problem unregistering this library: " + e.Message);
-                }
         }
 
         /// <summary>
-        /// generates an addin icon (.png) format and saves it on assembly folder
+        /// unregisters the addin once removed or when the project is cleaned
         /// </summary>
-        /// <returns>returns address</returns>
-        private static string GetIconPath(Type type, AddinAttribute addinAttribute)
+        /// <param name="type"></param>
+        public static void BaseUnregisterFunction(Type type)
         {
-            //get addin icon
-            var icon = AddinIcons.GetInstance(addinAttribute).GetAddinIcon(type);
-
-            //if could not get the icon from the addin attribute
-            if (icon == null)
-
-                //create a new empty one
-                icon = new Bitmap(16, 16);
-            
-            var dirInfo=AddinIcons.CreateIconsDir(addinAttribute.Title);
-            string addinIconAddress = Path.Combine(dirInfo.FullName, type.Name + ".png");
-
-            //resize, save and dispose of
-            using (icon)
+            try
             {
-                using (var resized = new Bitmap(icon, 16, 16))
-                {
-                    resized.Save(addinIconAddress);
-                }
+                string keyname = "SOFTWARE\\SolidWorks\\Addins\\{" + type.GUID.ToString() + "}";
+                Registry.LocalMachine.DeleteSubKey(keyname);
+
+                keyname = "Software\\SolidWorks\\AddInsStartup\\{" + type.GUID.ToString() + "}";
+                Registry.CurrentUser.DeleteSubKey(keyname);
             }
-            return addinIconAddress;
+            catch (Exception)
+            {
+            }
         }
     }
 }
