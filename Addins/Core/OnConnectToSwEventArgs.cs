0693c12e7ed7dae1d86792522a587e3005f3045a
H-Ashrafi <ashrafi@outlook.com.au>
2022-06-29 23:31:40 +1000

    move class to its own file

---
+++ Addins/Core/OnConnectToSwEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+
+namespace Hymma.Solidworks.Addins
+{
+    /// <summary>
+    /// event arguments for when solidworks connects or disconnects from your add-in
+    /// </summary>
+    public class OnConnectToSwEventArgs : EventArgs
+    {
+        /// <summary>
+        /// solidworks object
+        /// </summary>
+        public ISldWorks Solidworks { get; set; }
+
+        /// <summary>
+        /// the identifier for this addin
+        /// </summary>
+        public int Cookie { get; set; }
+    }
+}
