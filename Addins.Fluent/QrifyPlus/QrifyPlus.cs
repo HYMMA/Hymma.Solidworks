ed49a766f29f13e3b4d0bbc1c647837998e6a788
H-Ashrafi <ashrafi@outlook.com.au>
2022-07-09 00:58:15 +1000

    add essential cs files

--- Addins.Fluent/QrifyPlus/QrifyPlus.cs
+++ Addins.Fluent/QrifyPlus/QrifyPlus.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Hymma.Solidworks.Addins;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Hymma.Solidworks.Addins.Fluent;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using SolidWorks.Interop.swpublished;
 namespace QrifyPlus
 {
-    public class QrifyPlus
+    [Addin("QRify+",AddinIcon ="qrify",Description ="Generate QR Code and more",LoadAtStartup =true)]
+    public class QrifyPlus : AddinMaker
     {
+        public override AddinUserInterface GetUserInterFace()
+        {
+            var builder = new AddinModelBuilder();
+            builder
+                .AddCommandTab()
+                    .WithTitle("QRify+")
+                    .That()
+                    .IsVisibleIn(new[] {swDocumentTypes_e.swDocDRAWING })
+                .SaveCommandTab()
+                    .AddPropertyManagerPage("QRify",this.Solidworks)
+                        .AddTab("Generate QR",)
+                        .AddGroup("Generate QR")
+                        .
+                        .
+        }
     }
 }
