3f6cad09f5695d700a57595607bdc9ffa2d3178c
H-Ashrafi <ashrafi@outlook.com.au>
2022-07-09 00:35:07 +1000

    change qrify images

--- Addins/QRify/Properties/Resources.Designer.cs
+++ Addins/QRify/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace QRify.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap executeBtn {
-            get {
-                object obj = ResourceManager.GetObject("executeBtn", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap info {
             get {
                 object obj = ResourceManager.GetObject("info", resourceCulture);
