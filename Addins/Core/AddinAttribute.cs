923a7b3255ec2c474b1107b007624e6459cfdbbd
hamed <31099988+H-Ashrafi@users.noreply.github.com>
2024-05-24 23:25:13 +1000

    more comments

--- Addins/Core/AddinAttribute.cs
+++ Addins/Core/AddinAttribute.cs
@@ -18,7 +18,8 @@ namespace Hymma.Solidworks.Addins
         /// resource name of a bitmap icon in the <strong>.resx file</strong> <i>or</i> the filename (with extension) of an asset whose build property set to <strong>Embedded Resource</strong>.<br/> 
         /// The add-in icon displays next to the add-in name in the SOLIDWORKS Add-in Manager dialog<br/>
         /// </summary>
-        /// <remarks>any space character will be replaced by '_'</remarks>
+        /// <remarks>any space character will be replaced by '_'.<para> Don't expect the installer packages pick this up and create code to register the icon path into registry. Wix's heat (harvester tool) only registers the assemblies into the clients machine but you should register the addin icon yourself. refer to QrifyInstaller to see how it's done in Wix.</para></remarks>
+
         public string AddinIcon
         {
             get => _icon.Replace(" ", "_");
