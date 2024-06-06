4a2ea80e1d45d6d956c13d3dbc052b6074026baf
hamed <31099988+H-Ashrafi@users.noreply.github.com>
2024-06-07 09:40:00 +1000

    Created class
    this class helps with Path in dotnet

---
+++ Addins/Utilities/DotNet/PathHelpers.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (C) HYMMA All rights reserved.
+// Licensed under the MIT license
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymma.Solidworks.Addins.Utilities.DotNet
+{
+    /// <summary>
+    /// helpers methods for <see cref="Path"/>
+    /// </summary>
+    public static class PathHelpers
+    {
+        /// <summary>
+        /// removes invalid file name characters from file name
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>string without invalid file name chars</returns>
+        public static string RemoveInvalidFileNameChars(string input)
+        {
+            var sb = new StringBuilder(input);
+            var invalidFilaNameChars = Path.GetInvalidFileNameChars();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (invalidFilaNameChars.Contains(input[i]))
+                    sb.Remove(i, 1);
+            }
+            return sb.ToString();
+        }
+    }
+}
